7203999eaf666ad44522f1d4bf40921e94e3951b
bakuzan <imannoyed4days@hotmail.co.uk>
2019-09-02 17:04:19 +0100

    working downloader.

--- KarmaRanking/run.py
+++ KarmaRanking/run.py
@@ -9,6 +9,13 @@ from config import load_config
 
 logger = Logger("karma-log")
 
+season_names = {
+    'fall': '10',
+    'winter': '01',
+    'spring': '04',
+    'summer': '07'
+}
+
 
 def setup():
     load_config()
@@ -16,36 +23,47 @@ def setup():
 
 
 def make_filename_safe(filename):
-    keepcharacters = (' ', '.', '_')
+    keepcharacters = (' ', '.', '_', '-', '[', ']')
     return "".join(c for c in filename if c.isalnum() or c in keepcharacters).rstrip()
 
 
-def build_filename(title, ext):
-    title = title.split("|")[-1]
-    good_name = make_filename_safe(title)
+def build_filename(title, ext, suffix):
+    title = title.lower().split("|")[-1]
+
+    [week, left_bracket, seasonal] = title.partition('[')
+    [season, year] = seasonal.rpartition(']')[0].split(" ")
+
+    season_num = season_names[season]
+    week_num = week.strip().split(' ')[1].zfill(2)
+
+    filename_pattern = "[%s-%s_%s]_week-%s%s"
+    unsafe_name = filename_pattern % (year,
+                                      season_num, season, week_num, suffix)
+
+    good_name = make_filename_safe(unsafe_name)
     filename = good_name + "." + ext
     return abspath(join(dirname(__file__), "./downloads", filename))
 
 
 def do_search(sub, search):
-    print("Search subreddit with %s" % search)
-    for post in sub.search(search, sort="new"):
+    timeframe = os.environ.get("TIMEFRAME", "all")
+    print("Search subreddit with '%s' in timeframe %s" % (search, timeframe))
+    for post in sub.search(search, sort="new", time_filter=timeframe):
         is_imgur = "imgur" in post.url
         is_ireddit = "i.redd.it" in post.url
         is_image_post = is_imgur or is_ireddit
         if not is_image_post:
             continue
 
-        print("Title: %s" % post.title)
-        print("Url: %s" % post.url)
-
         if is_ireddit:
             ext = post.url.split(".")[-1]
-            filename = build_filename(post.title, ext)
+            filename = build_filename(post.title, ext, "")
             request.urlretrieve(post.url, filename=filename)
 
         if is_imgur:
-            print("TODO: Download imgur album")
+            ext = "zip"
+            filename = build_filename(post.title, ext, "-and-overview")
+            request.urlretrieve("%s/zip" % post.url, filename=filename)
 
 
 if __name__ == "__main__":
